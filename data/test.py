cf405ce3b8816e9981c7ab91f0bae421697a1615
Thomas Hansen <thomas.hansen@gmail.com>
2012-11-08 11:22:44 -0600

    test loading single county svg and render path as outline and polygib

---
+++ data/test.py
@@ -0,0 +1,53 @@
+from kivy.app import App
+from kivy.logger import Logger
+from kivy.uix.widget import Widget
+from kivy.uix.scatter import ScatterPlane
+from kivy.properties import StringProperty
+from kivy.graphics import Line, Triangle, Color
+from kivy.clock import Clock
+
+from xml.etree import ElementTree
+
+import p2t
+
+
+class SVGImage(ScatterPlane):
+    source = StringProperty("")
+
+    def on_source(self, *args):
+        Clock.schedule_once(self.parse_svg)
+
+    def parse_svg(self, *args):
+        self.svg_tree = ElementTree.parse(self.source)
+        self.svg_root = self.svg_tree.getroot()
+        for elem in self.svg_root:
+            if elem.tag == "path":
+                self.add_path(elem)
+
+    def add_path(self, svg_path):
+        height = float(self.svg_root.attrib['height'])
+        verts = map(eval, svg_path.attrib['d'][2:-2].split("L"))
+        verts = [(p[0], height-p[1]) for p in verts]
+        polyline = [p2t.Point(v[0], v[1]) for v in verts]
+        triangles = p2t.CDT(polyline).triangulate()
+        outline = sum(verts + [verts[0]], ())
+        with self.canvas:
+            Color(.3,.3,.3,1)
+            for t in triangles:
+                Triangle(points=(t.a.x, t.a.y, t.b.x, t.b.y, t.c.x, t.c.y))
+            Color(1,1,1,1)
+            Line(points=outline, width=1.2)
+
+
+
+
+
+
+
+
+class SVGTestApp(App):
+    def build(self):
+        return SVGImage(source="counties/Des_Moines.svg")
+
+
+SVGTestApp().run()
