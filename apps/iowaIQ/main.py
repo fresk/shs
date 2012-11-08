811e3504b381606870cc3732bc7fcae0e6ef0f44
Thomas Hansen <thomas.hansen@gmail.com>
2012-11-08 15:33:47 -0600

    start iowaiq screens

--- apps/iowaIQ/main.py
+++ apps/iowaIQ/main.py
@@ -1,96 +1,77 @@
 from kivy.app import App
-from kivy.clock import Clock
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.properties import *
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-import json
+import os
+import glob
 import random
 
 
-class QuizAnswer(Button):
-    text = StringProperty("Answer Text")
-    correct = BooleanProperty(False)
+class Question(object):
+    def __init__(self, **kwargs):
+        self.question = kwargs.get('question')
+        self.options = kwargs.get('options')
+        self.answer = kwargs.get('answer')
+        self.image = kwargs.get('image')
+
+    @classmethod
+    def load(cls, path):
+        qargs = {}
+        fname = os.path.join(path, 'question.txt')
+        qargs['question'] = open(fname, 'r').read()
+
+        fname = os.path.join(path, 'answer.txt')
+        qargs['answer'] = open(fname, 'r').read()
 
 
+        fname = os.path.join(path, 'options.txt')
+        qargs['options'] = []
+        for opt in open(fname, 'r').readlines():
+            if opt.startswith('!'):
+                opt = opt[1:]
+                qargs['options'].append((opt, True))
+            else:
+                qargs['options'].append((opt, False))
 
-class QuizScreen(FloatLayout):
-    question = StringProperty("Question")
-    explanation = StringProperty("Explanation")
-    answers = ListProperty(None)
-
-    answerlayout = ObjectProperty(None)
-
-    def _add_answers(self, *args):
-        print self, self.answerlayout
-        self.answerlayout.clear_widgets()
-        for a in self.answers:
-            w = QuizAnswer(**a)
-            self.answerlayout.add_widget(w)
-
-    def on_answers(self, *args):
-        Clock.schedule_once(self._add_answers)
-
-
-
-class ExplanationScreen(FloatLayout):
-    answer = ObjectProperty(None)
-    explanation = StringProperty("Explanation")
-
+        return Question(**qargs)
 
 
 
 
+class IntroScreen(Screen):
+    pass
 
-class QuizApp(App):
-    def submit_answer(self, answer):
-        self.root.clear_widgets()
-        expl = ExplanationScreen(answer=answer, **self.quiz)
-        self.root.add_widget(expl)
 
-    def random_question(self, *args):
-        self.quiz = random.choice(self.questions)
-        self.root.clear_widgets()
-        self.root.add_widget(QuizScreen(**self.quiz))
+class PlayerInfoScreen(Screen):
+    pass
 
-    def load_data(self):
-        def _decode_list(data):
-            rv = []
-            for item in data:
-                if isinstance(item, unicode):
-                    item = item.encode('utf-8')
-                elif isinstance(item, list):
-                    item = _decode_list(item)
-                elif isinstance(item, dict):
-                    item = _decode_dict(item)
-                rv.append(item)
-            return rv
 
-        def _decode_dict(data):
-            rv = {}
-            for key, value in data.iteritems():
-                if isinstance(key, unicode):
-                   key = key.encode('utf-8')
-                if isinstance(value, unicode):
-                   value = value.encode('utf-8')
-                elif isinstance(value, list):
-                   value = _decode_list(value)
-                elif isinstance(value, dict):
-                   value = _decode_dict(value)
-                rv[key] = value
-            return rv
+class QuestionScreen(Screen):
+    pass
 
-        self.questions = json.load(open('questions.json', 'r'), object_hook=_decode_dict)
 
+class AnswerScreen(Screen):
+    pass
+
+
+class ResultsScreen(Screen):
+    pass
+
+
+
+class IowaIQApp(App):
+
+    def load_questions(self):
+        self.questions = [Question.load(q) for q in glob.glob('questions/*')]
 
     def build(self):
-        self.load_data()
-        Clock.schedule_once(self.random_question)
-        return FloatLayout()
+        self.load_questions()
+        self.screen_manager = ScreenManager()
+        self.screen_manager.add_widget(IntroScreen(name='intro'))
+        self.screen_manager.add_widget(PlayerInfoScreen(name='playerinfo'))
+        self.screen_manager.add_widget(QuestionScreen(name='question'))
+        self.screen_manager.add_widget(AnswerScreen(name='answer'))
+        self.screen_manager.add_widget(ResultsScreen(name='results'))
 
+        return self.screen_manager
 
-
-
-if __name__ == "__main__":
-    QuizApp().run()
+IowaIQApp().run()
