67b4036d9e78cc21cdf322e5e709109aebc18097
Thomas Hansen <thomas.hansen@gmail.com>
2012-11-10 18:12:46 -0600

    game logic

--- apps/iowaIQ/main.py
+++ apps/iowaIQ/main.py
@@ -1,26 +1,30 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager, SlideTransition
+from kivy.properties import ObjectProperty, StringProperty, BooleanProperty
 
 import os
 import glob
 import random
 
 
+
+
 class Question(object):
     def __init__(self, **kwargs):
-        self.question = kwargs.get('question')
-        self.options = kwargs.get('options')
-        self.answer = kwargs.get('answer')
-        self.image = kwargs.get('image')
+        self.text = kwargs.get('text', "")
+        self.options = kwargs.get('options', (("", 0),)*4)
+        self.answer = kwargs.get('answer', "")
+        self.detail = kwargs.get('detail', "")
+        self.image = kwargs.get('image', "")
 
     @classmethod
     def load(cls, path):
         qargs = {}
         fname = os.path.join(path, 'question.txt')
-        qargs['question'] = open(fname, 'r').read()
+        qargs['text'] = open(fname, 'r').read()
 
         fname = os.path.join(path, 'answer.txt')
-        qargs['answer'] = open(fname, 'r').read()
+        qargs['detail'] = open(fname, 'r').read()
 
 
         fname = os.path.join(path, 'options.txt')
@@ -28,9 +32,8 @@ class Question(object):
         for opt in open(fname, 'r').readlines():
             if opt.startswith('!'):
                 opt = opt[1:]
-                qargs['options'].append((opt, True))
-            else:
-                qargs['options'].append((opt, False))
+                qargs['answer'] = opt
+            qargs['options'].append(opt)
 
         return Question(**qargs)
 
@@ -46,15 +49,20 @@ class PlayerInfoScreen(Screen):
 
 
 class QuestionScreen(Screen):
-    pass
+    text = StringProperty("")
+    option_a = StringProperty("")
+    option_b = StringProperty("")
+    option_c = StringProperty("")
+    option_d = StringProperty("")
 
 
 class AnswerScreen(Screen):
-    pass
+    text = StringProperty("")
+    correct = BooleanProperty(False)
 
 
 class ResultsScreen(Screen):
-    pass
+    text = StringProperty("")
 
 
 
@@ -62,6 +70,37 @@ class IowaIQApp(App):
 
     def load_questions(self):
         self.questions = [Question.load(q) for q in glob.glob('questions/*')]
+
+    def start_quiz(self):
+        self.score = 0
+        self.quiz = random.sample(self.questions, 4)
+        self.next_question()
+
+    def next_question(self):
+        if not self.quiz:
+            return self.finish_quiz()
+        q = self.question = self.quiz.pop()
+        qscreen = self.screen_manager.get_screen('question')
+        qscreen.text = q.text
+        qscreen.option_a = q.options[0]
+        qscreen.option_b = q.options[1]
+        qscreen.option_c = q.options[2]
+        qscreen.option_d = q.options[3]
+        self.screen_manager.current = 'question'
+
+    def check_answer(self, answer):
+        ascreen = self.screen_manager.get_screen('answer')
+        if (answer == self.question.answer):
+            self.score += 1
+        ascreen.text = self.question.detail
+        ascreen.correct = (answer == self.question.answer)
+        self.screen_manager.current = 'answer'
+
+
+    def finish_quiz(self):
+        rscreen = self.screen_manager.get_screen('results')
+        rscreen.text = "You got {0} out of 4".format(self.score)
+        self.screen_manager.current = 'results'
 
     def build(self):
         self.load_questions()
@@ -71,7 +110,7 @@ class IowaIQApp(App):
         self.screen_manager.add_widget(QuestionScreen(name='question'))
         self.screen_manager.add_widget(AnswerScreen(name='answer'))
         self.screen_manager.add_widget(ResultsScreen(name='results'))
-
         return self.screen_manager
 
-IowaIQApp().run()
+APP = IowaIQApp()
+APP.run()
--- apps/iowaIQ/run.sh
+++ apps/iowaIQ/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPTPATH=`dirname $0`
 echo $SCRIPTPATH
-python ${SCRIPTPATH}/main.py
+python ${SCRIPTPATH}/main.py --size=1024x768
