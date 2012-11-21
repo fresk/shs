270f82fd3bbcfc81ab1fb69807249a2a66edf2cd
Mathieu Virbel <mat@kivy.org>
2012-11-21 02:48:23 +0100

    use UrlRequest logic for downloading questions.json and associated data. TODO: optimize UrlRequest to use httplib2 if available, and found a way to batch requests.

--- apps/iowaIQ/jsondata.py
+++ apps/iowaIQ/jsondata.py
@@ -1,59 +1,187 @@
-import os
+from kivy.clock import Clock
 import json
 import urllib2
-import httplib2
+from os.path import join, exists
+from kivy.network.urlrequest import UrlRequest
+from kivy.app import App
 
 
-def load(fname):
-    return json.load(open(fname, 'r'), object_hook=_decode_dict)
+class JsonData(object):
 
-def _download_resource(url):
-    h = httplib2.Http()
-    url = urllib2.quote(url, safe=':/')
-    resp, content = h.request(url, "HEAD")
-    if resp['status'] > '399':
-        raise Exception("HTTP ERROR: %s"%resp)
+    def __init__(self, fname, on_success, on_progress):
+        '''Load a json, and return the result in the `on_success` callback.
 
-    ext = resp['content-type'].split('/')[-1]
-    cache_name = "resources/{0}.{1}".format(resp['etag'][1:-1], ext)
-    if os.path.exists(cache_name):
-        return cache_name
+        :Parameters:
+            `on_success`: result
+                Return the result of the json load.
+            `on_progress`: message, size, total
+                Called everytime a file is downloaded.
+        '''
 
-    resp, content = h.request(url, "GET")
-    if resp['status'] > '399':
-        raise Exception("HTTP ERROR: %s"%resp)
-    fout = open(cache_name, 'w').write(content)
+        super(JsonData, self).__init__()
+
+        self.cb_success = on_success
+        self.cb_progress = on_progress
+        self.fname = fname
+
+        # (internal) mode used when json is decoded
+        self._json_mode = 'detect' # detect or replace
+        # (internal) map one url to one local filename
+        self._json_cache = {}
+        # (internal) list of all url detected in the json
+        self._json_urls = []
+        # (internal) current http request
+        self._json_req = None
+        # (internal) url count to load
+        self._json_count = 0
+
+        # first step, list all the resources we need to check
+        self._json_mode = 'detect'
+        with open(fname, 'r') as fd:
+            json.load(fd, object_hook=self._decode_dict)
+
+        print 'Detected {0} urls'.format(len(self._json_urls))
+        self._json_count = len(self._json_urls)
+        Clock.schedule_interval(self._download_resources, 0)
+
+    def _replace_resources(self):
+        # second step, called when all the download are done
+        self._json_mode = 'replace'
+        with open(self.fname, 'r') as fd:
+            result = json.load(fd, object_hook=self._decode_dict)
+        self.cb_success(result)
+
+    def _download_resources(self, *args):
+        # we finished to download all the resources. now replace.
+        if self._json_req is None and not self._json_urls:
+            self._replace_resources()
+            return False
+
+        # we are still downloading one resource
+        if self._json_req is not None:
+            return
+
+        # start a resource download
+        url = urllib2.quote(self._json_urls.pop(), safe=':/')
+        self.cb_progress('Downloading resources {0}/{1}'.format(
+            self._json_count - len(self._json_urls), self._json_count),
+            self._json_count - 1 - len(self._json_urls),
+            self._json_count)
+        self._json_req = UrlRequest(url,
+                on_success=self._on_dhead_success,
+                on_error=self._on_dhead_error,
+                method='HEAD')
+
+    def _on_dhead_success(self, req, *args):
+        if req.resp_status > 399:
+            raise Exception('HTTP HEAD ERROR: {0} ({1})'.format(
+                req.url, req.resp_status))
+
+        url = urllib2.unquote(req.url)
+        etag = req.resp_headers['etag'][1:-1]
+        ext = req.resp_headers['content-type'].split('/')[-1]
+
+        # link the url to a local filename
+        local_fn = join(App.get_running_app().get_data_dir(),
+                'resources', '{0}.{1}'.format(etag, ext))
+        self._json_cache[url] = local_fn
+
+        # if the local file is not found, get it.
+        if not exists(local_fn):
+            self._json_req = UrlRequest(urllib2.quote(url, safe=':/'),
+                    on_success=self._on_dget_success,
+                    on_progress=self._on_dget_progress,
+                    on_error=self._on_dget_error)
+            return
+
+        # otherwise, just release the req, the next download will happen in the
+        # clock.
+        self._json_req = None
+
+    def _on_dhead_error(self, *args):
+        self._json_req = None
+
+    def _on_dget_success(self, req, result, *args):
+        url = urllib2.unquote(req.url)
+        local_fn = self._json_cache[url]
+        if req.resp_status > 399:
+            raise Exception('HTTP GET ERROR: {0} ({1})'.format(
+                req.url, req.resp_status))
+        with open(local_fn, 'wb') as fd:
+            fd.write(result)
+
+        # release the req, the next download will happen in the clock.
+        self._json_req = None
+
+    def _on_dget_error(self, *args):
+        # TODO
+        pass
+
+    def _on_dget_progress(self, req, cursize, total):
+        step = (cursize / float(total)) if total > 0 else 0
+        self.cb_progress('Downloading resources {0}/{1}'.format(
+            self._json_count - len(self._json_urls), self._json_count),
+            self._json_count - 1 - len(self._json_urls) + step,
+            self._json_count)
+
+    def _download_resource(self, url):
+        if self._json_mode == 'detect':
+            if url not in self._json_urls:
+                self._json_urls.append(url)
+            return url
+        if self._json_mode == 'replace':
+            # XXX use a missing image if not found
+            return self._json_cache.get(url)
+
+
+    def _decode_list(self, data):
+        rv = []
+        for item in data:
+            if isinstance(item, unicode):
+                item = item.encode('utf-8')
+                if item.startswith("http://"):
+                    item = self._download_resource(item)
+            elif isinstance(item, list):
+                item = self._decode_list(item)
+            elif isinstance(item, dict):
+                item = self._decode_dict(item)
+            rv.append(item)
+        return rv
+
+
+    def _decode_dict(self, data):
+        rv = {}
+        for key, value in data.iteritems():
+            if isinstance(key, unicode):
+                key = key.encode('utf-8')
+            if isinstance(value, unicode):
+                value = value.encode('utf-8')
+                if value.startswith("http://"):
+                    value = self._download_resource(value)
+            elif isinstance(value, list):
+                value = self._decode_list(value)
+            elif isinstance(value, dict):
+                value = self._decode_dict(value)
+            rv[key] = value
+        return rv
+
+
+'''
+# original code
+h = httplib2.Http()
+url = urllib2.quote(url, safe=':/')
+resp, content = h.request(url, "HEAD")
+if resp['status'] > '399':
+    raise Exception("HTTP ERROR: %s"%resp)
+
+ext = resp['content-type'].split('/')[-1]
+cache_name = "resources/{0}.{1}".format(resp['etag'][1:-1], ext)
+if os.path.exists(cache_name):
     return cache_name
 
-
-def _decode_list(data):
-    rv = []
-    for item in data:
-        if isinstance(item, unicode):
-            item = item.encode('utf-8')
-            if item.startswith("http://"):
-                item = _download_resource(item)
-        elif isinstance(item, list):
-            item = _decode_list(item)
-        elif isinstance(item, dict):
-            item = _decode_dict(item)
-        rv.append(item)
-    return rv
-
-
-def _decode_dict(data):
-    rv = {}
-    for key, value in data.iteritems():
-        if isinstance(key, unicode):
-            key = key.encode('utf-8')
-        if isinstance(value, unicode):
-            value = value.encode('utf-8')
-            if value.startswith("http://"):
-                value = _download_resource(value)
-        elif isinstance(value, list):
-            value = _decode_list(value)
-        elif isinstance(value, dict):
-            value = _decode_dict(value)
-        rv[key] = value
-    return rv
-
+resp, content = h.request(url, "GET")
+if resp['status'] > '399':
+    raise Exception("HTTP ERROR: %s"%resp)
+fout = open(cache_name, 'w').write(content)
+return cache_name
+'''
--- apps/iowaIQ/main.py
+++ apps/iowaIQ/main.py
@@ -1,17 +1,25 @@
-import os
-import md5
 import random
-import urllib2
 import json
-import jsondata
-import requests
+from jsondata import JsonData
 
 
+from os import makedirs
+from os.path import join, exists
 from kivy.app import App
 from kivy.uix.image import Image
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.modalview import ModalView
 from kivy.uix.screenmanager import Screen, ScreenManager, SlideTransition
+from kivy.network.urlrequest import UrlRequest
 from kivy.properties import *
+from kivy.utils import platform
+from kivy.clock import Clock
 from viewport import Viewport
+
+
+class ProgressionView(ModalView):
+    text = StringProperty()
+    progression = NumericProperty()
 
 
 class IntroScreen(Screen):
@@ -58,15 +66,11 @@ class ResultsScreen(Screen):
 
 class IowaIQApp(App):
 
-    def pull_update(self):
-        r = requests.get('http://www.fresksite.net/dcadb/wp-content/themes/dca/api/questions.php')
-        if r.status_code == 200:
-            json.dump(r.json, open('questions.json', 'w'))
-    def load_questions(self):
-        self.pull_update()
-        self.questions = jsondata.load('questions.json')
-        for q in self.questions:
-            print q['question_bg_image']
+    def build_config(self, config):
+        config.setdefaults('app', {
+            'questions':
+                'http://www.fresksite.net/dcadb/wp-content/themes/dca/api/questions.php'
+        })
 
     def start_quiz(self):
         self.score = 0
@@ -104,7 +108,11 @@ class IowaIQApp(App):
         self.screen_manager.current = 'results'
 
     def build(self):
+        self.root = FloatLayout()
         self.load_questions()
+
+    def show_app(self, *args):
+        self._hide_progression()
         self.screen_manager = ScreenManager(transition=SlideTransition())
         self.screen_manager.add_widget(IntroScreen(name='intro'))
         self.screen_manager.add_widget(QuestionScreen(name='question'))
@@ -113,7 +121,71 @@ class IowaIQApp(App):
 
         self.viewport = Viewport(size=(2048,1536))
         self.viewport.add_widget(self.screen_manager)
-        return self.viewport
+
+        self.root.add_widget(self.viewport)
+
+    def get_data_dir(self):
+        if platform() == 'ios':
+            directory = join(self.directory, 'Documents')
+        elif platform() == 'android':
+            directory = join('/sdcard', '.{0}'.format(self.get_application_name))
+        else:
+            directory = self.directory
+        if not exists(directory):
+            makedirs(directory)
+        return directory
+
+    #
+    # Update part
+    # Manage the update of questions.json + associated data
+    #
+    def load_questions(self):
+        # first step, download the questions.json
+        self._req = UrlRequest(self.config.get('app', 'questions'),
+                on_success=self._pull_update_2,
+                on_error=self._pull_update_failed,
+                on_progress=lambda req, cursize, totalsize: self._show_progression(
+                    'Downloading questions...', cursize, totalsize))
+
+    def _pull_update_2(self, request, result):
+        # second step, download all the outdated resources
+        self._questions_fn = join(self.get_data_dir(), 'questions.json')
+        with open(self._questions_fn, 'w') as fd:
+            json.dump(result, fd)
+
+        self._jsondata = JsonData('questions.json',
+                on_success=self._pull_update_3,
+                on_progress=self._show_progression)
+
+    def _pull_update_3(self, questions):
+        self.questions = questions
+        for q in self.questions:
+            print q['question_bg_image']
+        self._show_progression('Starting application...', 100, 100)
+        Clock.schedule_once(self.show_app, 0.1)
+
+    def _pull_update_failed(self, request, error):
+        # XXX TODO
+        pass
+
+    #
+    # Modal view for showing progression
+    #
+    def _show_progression(self, text, size, total):
+        if not hasattr(self, '_progression'):
+            self._progression = ProgressionView()
+            self._progression.open()
+        self._progression.text = text
+        if total > 0:
+            self._progression.progression = size / float(total) * 100.
+        else:
+            self._progression.progression = 0
+
+    def _hide_progression(self, *largs):
+        if hasattr(self, '_progression'):
+            self._progression.dismiss()
+            del self._progression
+
 
 APP = IowaIQApp()
 APP.run()
